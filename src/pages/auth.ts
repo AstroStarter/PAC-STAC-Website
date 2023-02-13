505fe12504d81d59c3de9cc6e2bf73a9915df8f1
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-13 04:07:50 +0200

    Add import for AstroAuthConfig from "auth-astro"

--- src/pages/auth.ts
+++ src/pages/auth.ts
@@ -1,12 +1,13 @@
 import { AstroAuth } from "auth-astro";
 import type { AstroAuthConfig } from "auth-astro";
 
+// @ts-ignore
 import GitHub from "@auth/core/providers/github";
 import env from "@lib/env.js";
 
 export const authOpts: AstroAuthConfig = {
 	providers: [
-		//@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
+		// @ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
 		GitHub({
 			clientId: env.GITHUB_ID,
 			clientSecret: env.GITHUB_SECRET,
