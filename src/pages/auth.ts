ac98f43afe2085e37914d942f5c887c26ae5a27e
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-21 03:32:26 +0200

    squash!

--- src/pages/auth.ts
+++ src/pages/auth.ts
@@ -7,7 +7,6 @@ import env from "@lib/env.js";
 
 export const authOpts: AstroAuthConfig = {
 	providers: [
-		// @ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
 		GitHub({
 			clientId: env.GITHUB_ID,
 			clientSecret: env.GITHUB_SECRET,
