382bb94a27c849997e629d424995e900c52a143c
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-05 04:04:21 +0300

    squash!

--- Source/Library/Environment.ts
+++ Source/Library/Environment.ts
@@ -5,13 +5,9 @@ dotenv.config();
 
 export default z
 	.object({
-<<<<<<< HEAD
 		AUTH_SECRET: z.string().default(""),
 		AUTH_TRUST_HOST: z.string().default(""),
 		GITHUB_ID: z.string().default(""),
 		GITHUB_SECRET: z.string().default(""),
-=======
-		GH_AUTH_TOKEN: z.string().default(""),
->>>>>>> upstream/main
 	})
 	.parse(process.env);
